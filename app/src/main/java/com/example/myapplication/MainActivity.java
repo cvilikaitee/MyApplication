2ee1020773334ba2861d63e9cd0f9c1935f51077
cvilikaitee <evelina.cvilikaite@gmail.com>
2021-09-29 10:17:07 +0300

    Add buttons and text color

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,13 +2,45 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView OurText;
+    Button TheButton;
+    Button TheButton2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //setting the variables to the Layout file
+        OurText = findViewById(R.id.textView);
+        TheButton = findViewById(R.id.button);
+
+        //Setting the OnClick Listener
+        TheButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // What we want to display when button is clicked
+                OurText.setText("The Button was Clicked!");
+            }
+        });
+        //setting the variables to the Layout file
+        TheButton2 = findViewById(R.id.button2);
+
+        //Setting the OnClick Listener
+        TheButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // What we want to display when button is clicked
+                OurText.setTextColor(Color.rgb(200,0,0));
+            }
+        });
     }
 }
